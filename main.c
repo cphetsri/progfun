bacbae551935684f77234f22d9b14286459f19c8
cphetsri <68010233@kmitl.ac.th>
2025-09-22 07:58:05 +0000

    set a original data (that Prof. gave) in csv file and make function (that detect when file csv and use or not ) in main.c file

--- main.c
+++ main.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    printf("===================================================================\n\n");
+
+    FILE *brwdata = fopen("borrowdfsdata.csv", "r");
+    if (brwdata == NULL) {
+        printf("################ Error: could not open csv file ################### \n\n-------- File name doesn't match or That file doesn't exit --------\n\n");
+        printf("===================================================================\n");
+        exit(-1);
+    }
+
+    printf("I was able to open the file\n");
+    printf("===================================================================\n");
+}
+
 // บันทึกข้อมูลการยืมอุปกรณ์ในสำนักงาน ลงในไฟล์ csv
