ab77d193f9034eca2d4b6d9f2020c193777b6146
cphetsri <68010233@kmitl.ac.th>
2025-09-29 17:10:16 +0000

    del readme.md for looking and try to open files csv and delete but not finish

--- README.md
+++
@@ -1,2 +0,0 @@
-# progfun
-this repo is for my Project in Program fundimentals subject of KMITL. : )
--- main.c
+++ main.c
@@ -2,17 +2,39 @@
 #include <stdlib.h>
 
 int main(){
+
     printf("===================================================================\n\n");
 
-    FILE *brwdata = fopen("borrowdfsdata.csv", "r");
+    FILE *brwdata = fopen("borrowdata.csv", "r");
     if (brwdata == NULL) {
         printf("################ Error: could not open csv file ################### \n\n-------- File name doesn't match or That file doesn't exit --------\n\n");
         printf("===================================================================\n");
         exit(-1);
     }
 
-    printf("I was able to open the file\n");
+    printf("I was able to open the file\n\n");
     printf("===================================================================\n");
+
+
+
+
+    // FILE *fop;
+    // fop = fopen("borrowdata.csv", "a");
+    // if (fop == NULL) {
+    //     printf("Error when opening the file!\n");
+    //     return 1;
+    // }
+
+    // fprintf(fop, "Johnny Depp, projecttor, 2025-08-01, 2025-08-29");
+
+    // fclose(fop);
+    // printf("\nAlready added.");
+    // return 0;
+
+
+
+    
+
 }
 
 // บันทึกข้อมูลการยืมอุปกรณ์ในสำนักงาน ลงในไฟล์ csv
