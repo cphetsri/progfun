508d84e7912600d30645a10ac8b91effa5530ae9
cphetsri <68010233@kmitl.ac.th>
2025-09-30 14:11:08 +0000

    make the main menu

--- main.c
+++ main.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(){
 
-    printf("===================================================================\n\n");
+    char buffer[10000];
+    char *emp_name;
+    char *eqm_name;
+    char *brw_date;
+    char *rtn_date;
 
+    printf("===================================================================\n\n");
     FILE *brwdata = fopen("borrowdata.csv", "r");
     if (brwdata == NULL) {
         printf("################ Error: could not open csv file ################### \n\n-------- File name doesn't match or That file doesn't exit --------\n\n");
@@ -17,6 +23,28 @@ int main(){
 
 
 
+    fgets(buffer, sizeof(buffer), brwdata);
+    // printf("\nThe first line is :\n%s\n", buffer);
+
+    while(fgets(buffer, sizeof(buffer), brwdata)) {
+        // printf("The rest of the line is :\n%s\n", buffer);
+
+        emp_name = strtok(buffer, ",");
+        printf("Employee Name : %s\n", emp_name);
+        
+        eqm_name = strtok(NULL, ",");
+        printf("Equipment that they need : %s\n", eqm_name);
+
+        brw_date = strtok(NULL, ",");
+        printf("Borrow date : %s\n", brw_date);
+
+        rtn_date = strtok(NULL, ",");
+        printf("Return date : %s\n", rtn_date);
+    }
+
+    
+
+
 
     // FILE *fop;
     // fop = fopen("borrowdata.csv", "a");
@@ -25,10 +53,10 @@ int main(){
     //     return 1;
     // }
 
-    // fprintf(fop, "Johnny Depp, projecttor, 2025-08-01, 2025-08-29");
+    // fprintf(fop, "\"Johnny Depp\", \"projecttor\", \"2025-08-01\", \"2025-08-29\"");
 
     // fclose(fop);
-    // printf("\nAlready added.");
+    // printf("\nAlready added.\n");
     // return 0;
 
 
