f734fde30294c95d02b6dd884e44da180eaa4fd5
cphetsri <68010233@kmitl.ac.th>
2025-10-02 01:05:16 +0000

    mess with code a bit

--- main.c
+++ main.c
@@ -24,7 +24,6 @@ int main(){
     printf("\n===================================================================\n\n");
     printf("What do you want to do [input the number] : ");
     scanf("%d", &decide);
-    // printf("%d\n", decide);
 
     if (decide == 1){
         printf("\n===================================================================\n\n");
@@ -44,17 +43,6 @@ int main(){
         while(fgets(buffer, sizeof(buffer), brwdata)) {
             printf("%s", buffer);
 
-            // emp_name = strtok(buffer, ",");
-            // printf("Employee Name : %s\n", emp_name);
-
-            // eqm_name = strtok(NULL, ",");
-            // printf("Equipment that they need : %s\n", eqm_name);
-
-            // brw_date = strtok(NULL, ",");
-            // printf("Borrow date : %s\n", brw_date);
-
-            // rtn_date = strtok(NULL, ",");
-            // printf("Return date : %s\n", rtn_date);
         }
         printf("\n");
 
@@ -119,7 +107,6 @@ int main(){
             char *token = strtok(line_copy, ",");
             while (token != NULL) {
                 if (strstr(token, keyword) != NULL) {
-                    // printf("Found '%s' in data base ---> %s\n", keyword, line);
                     found = 1;
                     emp_name = strtok(line, ",");
                     printf("Employee Name : %s\n", emp_name);
@@ -132,7 +119,6 @@ int main(){
 
                     rtn_date = strtok(NULL, ",");
                     printf("Return date : %s\n", rtn_date);
-                    // break;
                 }
                 token = strtok(NULL, ",");
             }
@@ -147,25 +133,63 @@ int main(){
 
     } else if (decide == 4){
         printf("\n===================================================================\n\n");
-        FILE *srchdata = fopen("borrowdata.csv", "r");
-        if (srchdata == NULL) {
-            perror("-Error : Cause by worng name of CSV file or Don't have CSV file!!-\n");
-            printf("===================================================================\n");
+
+        FILE *brwdata = fopen("borrowdata.csv", "r");
+        if (brwdata == NULL) {
+            printf("################ Error: could not open csv file ###################\n");
             return 1;
         }
 
-        FILE *tempFile = fopen("borrowdata.csv", "w");
+        FILE *tempFile = fopen("temp.csv", "w");
         if (tempFile == NULL) {
-            fclose(srchdata);
+            fclose(brwdata);
+            printf("################ Error: could not create temp file ################\n");
+            return 1;
         }
 
+        char keyword[200];
         char line[LINE_SIZE];
+        int found = 0;
 
-        while (fgets(line, sizeof(line), fp)) {
+        printf("Enter employee name you want to edit: ");
+        scanf(" %[^\n]", keyword);
 
+        while (fgets(line, sizeof(line), brwdata)) {
+            char line_copy[LINE_SIZE];
+            strcpy(line_copy, line);
+
+            char *emp = strtok(line_copy, ",");
+            if (emp != NULL && strcmp(emp, keyword) == 0) {
+                found = 1;
+                char newEqm[100], newBrw[20], newRtn[20];
+
+                printf("Found %s\n", keyword);
+                printf("Enter NEW Equipment : ");
+                scanf(" %99s", newEqm);
+
+                printf("Enter NEW Borrow Date [YYYY-MM-DD] : ");
+                scanf(" %19s", newBrw);
+
+                printf("Enter NEW Return Date [YYYY-MM-DD] : ");
+                scanf(" %19s", newRtn);
+
+                fprintf(tempFile, "%s, %s, %s, %s\n", emp, newEqm, newBrw, newRtn);
+            } else {
+                fputs(line, tempFile);
+            }
         }
 
+        fclose(brwdata);
+        fclose(tempFile);
 
+        if (!found) {
+            printf("Not found '%s' in database.\n", keyword);
+            remove("temp.csv");
+        } else {
+            remove("borrowdata.csv");
+            rename("temp.csv", "borrowdata.csv");
+            printf("Edit complete!\n");
+        }
 
 
 
